dde3c8f860e4ba94e8e5fb3bb01722ef01c57321
Rafal <rf.zajac@gmail.com>
2023-10-28 17:39:38 +0200

    displaying without much styling some user information on the dashboard

--- client/src/views/Dashboard.tsx
+++ client/src/views/Dashboard.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Container } from "react-bootstrap";
 
-type Props = {};
-
-function Dashboard({}: Props) {
-  return <div>Dashboard</div>;
+function Dashboard() {
+  const { user } = useContext(AuthContext);
+  return (
+    <>
+      <Container>
+        <h1>Welcome : {user?.userName}</h1>
+        <p>Your email: {user?.email}</p>
+        <img src={user?.userImage} alt="userImage" width={"150px"} />
+      </Container>
+    </>
+  );
 }
 
 export default Dashboard;
