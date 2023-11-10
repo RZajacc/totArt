a1b09bf9ebdf7aee45af43eefa53cdeeb7f58e13
Rafal <rf.zajac@gmail.com>
2023-11-10 14:29:44 +0100

    Added field validation for user register

--- client/src/components/UserRegister.tsx
+++ client/src/components/UserRegister.tsx
@@ -1,5 +1,5 @@
 import { Button, Container, Form } from "react-bootstrap";
-import { ChangeEvent, FormEvent, useState, useContext } from "react";
+import { ChangeEvent, FormEvent, useState, useContext, useEffect } from "react";
 import { User } from "../types/types";
 import "../styles/accountPage.css";
 import { AuthContext } from "../context/AuthContext";
@@ -14,17 +14,26 @@ function Register({ setLogReg }: Props) {
 
   // *1. Setting up a new user from input fields
   const [newUser, setNewUser] = useState<User>({
+    _id: "",
     userName: "",
     email: "",
     password: "",
     userImage: "",
     userWebsite: "",
     userBio: "",
+    favs: [""],
+    posts: [""],
   });
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordErr, setPasswordErr] = useState<string[] | null>(null);
 
   // *2. Receiving input from a form
   const handleInputData = (e: ChangeEvent<HTMLInputElement>) => {
     setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  };
+
+  const handleConfirmPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
   };
 
   // *3. Change displayed element on the page
@@ -35,8 +44,41 @@ function Register({ setLogReg }: Props) {
   // *4 Register a new user
   const handleRegisterSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    registerWithEmail(newUser);
-    setLogReg("login");
+    const formValidation: string[] = [];
+
+    if (newUser.password !== confirmPassword) {
+      formValidation.push("Provided passwords don't match!");
+    } else {
+      if (newUser.password.length < 8 || confirmPassword.length < 8) {
+        formValidation.push("Password is too short!");
+      }
+
+      if (!/[A-Z]/.test(newUser.password) || !/[A-Z]/.test(confirmPassword)) {
+        formValidation.push(
+          "Password needs to have at least one uppercase letter!"
+        );
+      }
+
+      if (
+        !/[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g.test(newUser.password) ||
+        !/[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g.test(confirmPassword)
+      ) {
+        formValidation.push(
+          "Password needs to contain at least 1 special character!"
+        );
+      }
+
+      if (!/[0-9]/.test(newUser.password) || !/[0-9]/.test(confirmPassword)) {
+        formValidation.push("Password needs to contain at least one number!");
+      }
+    }
+
+    setPasswordErr(formValidation);
+
+    if (passwordErr?.length === 0) {
+      registerWithEmail(newUser);
+      setLogReg("login");
+    }
   };
 
   return (
@@ -60,6 +102,7 @@ function Register({ setLogReg }: Props) {
               name="userName"
               placeholder="Enter username"
               onChange={handleInputData}
+              required
             />
           </Form.Group>
 
@@ -70,10 +113,8 @@ function Register({ setLogReg }: Props) {
               name="email"
               placeholder="Enter email"
               onChange={handleInputData}
+              required
             />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="user-password">
@@ -84,13 +125,43 @@ function Register({ setLogReg }: Props) {
               placeholder="Password"
               onChange={handleInputData}
               autoComplete="user-password"
+              isInvalid={passwordErr?.length != null && passwordErr.length != 0}
+              isValid={passwordErr?.length === 0}
+              required
             />
           </Form.Group>
 
+          <Form.Group className="mb-3" controlId="confirm-password">
+            <Form.Label>Confirm password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleConfirmPassword}
+              autoComplete="user-password"
+              isInvalid={passwordErr?.length != null && passwordErr.length != 0}
+              isValid={passwordErr?.length === 0}
+              required
+            />
+            {passwordErr &&
+              passwordErr.map((err, idx) => {
+                return (
+                  <Form.Control.Feedback type="invalid" key={idx}>
+                    {err}
+                  </Form.Control.Feedback>
+                );
+              })}
+          </Form.Group>
           <div className="text-center">
             <Button variant="warning" type="submit">
               Register
             </Button>
+          </div>
+          <div className="password-req">
+            <p>*Password needs to have at least 8 characters</p>
+            <p>*Password needs to contain at least 1 uppercase character</p>
+            <p>*Password needs to contain at least 1 number</p>
+            <p>*Password needs to contain at least 1 special character</p>
           </div>
         </Form>
       </Container>
